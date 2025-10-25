8fef68541f5a8d24ed9fa7f97183f603195239ae
wuhuohanke <wuhuohanke@hotmail.com>
2025-10-25 19:18:48 +0800

    Add content of student info

--- server/app.js
+++ server/app.js
@@ -12,8 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.use('/', indexRouter);
-app.use(express.static(path.join(__dirname, '../public'), { index: false }));
 app.use('/users', usersRouter);
 
 export default app;
