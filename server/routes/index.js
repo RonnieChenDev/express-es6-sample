6a35a9c6a93cd986434722b8ea52440a682208ca
wuhuohanke <wuhuohanke@hotmail.com>
2025-10-25 17:43:23 +0800

    Add content of student info

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,12 @@ import express from 'express';
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'My Student ID is 22278096. Welcome to Express!',
-  });
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'World' });
+// });
+
+router.get('/', (req, res) => {
+  res.type('text/plain').send('My Student ID is 22278096. Welcome to Express!');
 });
 
 export default router;
